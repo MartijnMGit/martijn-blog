d7878c83c3e6052fa9a0d3afb5b0dcd935421615
Martin <martijnmotmans@gmail.com>
2025-04-26 22:36:00 +0100

    update main with db

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
